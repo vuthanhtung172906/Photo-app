1b6a417293e1aecfc18f256f56d7dc831519f7cc
vuthanhtung172906 <vuthanhtung172906@gmail.com>
2021-07-29 23:50:58 +0700

    Change default PORT of heroku

--- main.js
+++ main.js
@@ -47,7 +47,10 @@ router.render = (req, res) => {
   });
 };
 // Use default router
+
 server.use("/api", router);
-server.listen(3000, () => {
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
   console.log("JSON Server is running");
 });
